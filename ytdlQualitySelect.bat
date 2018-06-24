b20fabea8cf0bf078b3e679fda2b13767b2118ca
Schytheron <12500415+Schytheron@users.noreply.github.com>
2018-06-24 13:21:02 +0200

    Update ytdlQualitySelect.bat

--- ytdlQualitySelect.bat
+++ ytdlQualitySelect.bat
@@ -1,3 +1,4 @@
+@echo off
 set /p URL="Enter YouTube video link: "
 set /p height="Enter desired video quality (allowed values -> 144 | 240 | 360 | 480 | 720 | 1080 | 1440 | 2160 [4K] | 4320 [8K]): "
 set /p fps="Enter desired FPS (keep in mind that the FPS you select must be available for the video): "
