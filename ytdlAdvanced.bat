b88f1d58a8abeda65a8337118cf7f69987da50c2
Schytheron <12500415+Schytheron@users.noreply.github.com>
2019-04-21 16:08:45 +0200

    Fixed ytdlAdvanced.bat

--- ytdlAdvanced.bat
+++ ytdlAdvanced.bat
@@ -2,7 +2,7 @@
 SET /P URL="Enter YouTube Video URL: "
 SET /P arguments="Enter custom arguments: "
 ECHO.
-youtube-dl -o Downloads/%%(title)s.%%(ext)s %arguments%
+youtube-dl -o Downloads/%%(title)s.%%(ext)s %URL% %arguments%
 ECHO.
 PAUSE
 EXIT
